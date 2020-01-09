1731196c0508874c18989a6892b59f214dafeea2
Ronit Gupta <ronitgupta@ronits-air.andover.edu>
2020-01-09 13:09:15 -0500

    pls work

--- StockExchange.java
+++ StockExchange.java
@@ -1,10 +1,10 @@
-
+import java.util.*;
 import Stock;
 
 public class StockExchange {
 
     public StockExchange() {
-
+        HashMap<String, Stock> stockExchange = new HashMap<>();
     }
 
     public void listStock(String symbol, String name, double price) {
