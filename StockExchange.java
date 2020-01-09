304a8d6275fdfca7eb5b04d73a98d2d7159adab9
Ronit Gupta <ronitgupta@ronits-air.andover.edu>
2020-01-09 13:26:08 -0500

    stock exchange done?

--- StockExchange.java
+++ StockExchange.java
@@ -8,15 +8,18 @@ public class StockExchange {
     }
 
     public void listStock(String symbol, String name, double price) {
-        
+        Stock addStock = new Stock(symbol, name, price);
+        stockExchange.put(symbol, addStock);
     }
 
     public String getQuote(String symbol) {
-
+        Stock ourStock = stockExchange.get(symbol);
+        return ourStock.getQuote();
     }
 
     public void placeOrder(TradeOrder tradeOrder) {
-        
+        Stock stockOrder = stockExchange.get(tradeOrder.getSymbol);
+        stockOrder.placeOrder();
     }
 }
 
