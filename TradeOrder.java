b92cbfd51500816ccb6c33b5c0fb0e35559990b8
Ronit Gupta <ronitgupta@ronits-air.andover.edu>
2020-01-10 12:50:22 -0500

    trade order

---
+++ TradeOrder.java
@@ -0,0 +1,64 @@
+/*
+
+author: ronit gupta
+trade order object that contains information about the order
+
+*/
+
+import java.util.*;
+
+public class TradeOrder {
+
+    private Trader trader;
+    private String symbol;
+    private boolean buyOrder;
+    private boolean marketOrder;
+    private int numShares;
+    private double price;
+
+    public TradeOrder(Trader trader, String symbol, boolean buyOrder, boolean marketOrder, int numShares, double price) {
+        this.trader = trader;
+        this.symbol = symbol;
+        this.buyOrder = buyOrder;
+        this.marketOrder = marketOrder;
+        this.numShares = numShares;
+        this.price = price;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public int getShares() {
+        return numShares;
+    }
+
+    public String getSymbol() {
+        return symbol;
+    }
+
+    public Trader getTrader() {
+        return trader;
+    }
+
+    public boolean isBuy() {
+        return buyOrder;
+    }
+
+    public boolean isLimit() {
+        return !isMarket();
+    }
+
+    public boolean isMarket() {
+        return marketOrder;
+    }
+
+    public boolean isSell() {
+        return !isBuy();
+    }
+
+    public void subtractShares(int shares) {
+        numsShares = numShares - shares;
+    }
+    
+}
