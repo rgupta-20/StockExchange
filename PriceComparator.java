573a477bc9352674f6f5443ed0b06eead43ecf6c
dh2shin <38078743+dh2shin@users.noreply.github.com>
2020-01-10 13:01:16 -0500

    Add files via upload

---
+++ PriceComparator.java
@@ -0,0 +1,36 @@
+/**
+ * Author: Harry Shin
+ * Trader Class
+ */
+import java.lang.Math;
+import java.util.*;
+class PriceComparator implements Comparator<TradeOrder> {
+    private boolean ascending;
+    public PriceComparator() {
+        ascending = true;
+    }
+    public PriceComparator(boolean asc) {
+        ascending = asc;
+    }
+    public int compare(TradeOrder order1, TradeOrder order2) {
+        if (!(order1.isLimit()) && !(order2.isLimit())) {
+            return 0;
+        }
+        else if (!(order1.isLimit()) && order2.isLimit()) {
+            return -1;
+        }
+        else if (!(order2.isLimit()) && order1.isLimit()) {
+            return 1;
+        }
+        else {
+            double diff = Math.round((order1.getPrice() - order2.getPrice()) * 100.0) / 100.0;
+            if (ascending) {
+                return d;
+            }
+            else {
+                return -d;
+            }
+        }
+    }
+
+}
