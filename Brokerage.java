14fd9ec12981f9df6541097b11ed05c050346694
jdiodati20 <48535722+jdiodati20@users.noreply.github.com>
2020-01-12 13:38:35 -0500

    Add files via upload

---
+++ Brokerage.java
@@ -0,0 +1,53 @@
+import java.util.*;
+import StackExchange;
+import Trader;
+
+
+public class Brokerage {
+
+  public TreeMap<String, Trader> traderList;
+  public TreeMap<String, Trader> loggedOnTraders;
+
+  public Brokerage (StockExchange exchange) {
+    traderList = new TreeMap<String, Trader>();
+    loggedOnTraders = new TreeMap<String, Trader>();
+  }
+
+  public int addUser (java.lang.String name, java.lang.String password) {
+    if (name.length() < 4 || name.length() > 10) {
+      return -1;
+    } else if (password.length() < 2 || password.length() > 10) {
+      return -2;
+    } else if (traderList.containsKey(name)) {
+      return -3;
+    }
+    Trader add = new Trader(this.Brokerage, name, password);
+    traderList.put(name, add);
+    return 0;
+  }
+
+  public int login (java.lang.String name, java.lang.String password) {
+    if (!traderList.containsKey(name)) {
+      return -1;
+    } else if (loggedOnTraders.containsKey(name)) {
+      return -3;
+    } else if (traderList.get(name).getPassword().equals(password)) {
+      return -2;
+    }
+    Trader add = new Trader(this.Brokerage, name, password);
+    loggedOnTraders.put(name, add);
+    return 0;
+  }
+
+  public void logout (Trader trader) {
+    loggedOnTraders.remove(Trader.getName());
+  }
+
+  public void getQuote (java.lang.String symbol, Trader trader) {
+    trader.receiveMessage(Trader.getQuote(symbol));
+  }
+
+  public void placeOrder(TradeOrder order) {
+    StackExchange.placeOrder(order));
+  }
+}
